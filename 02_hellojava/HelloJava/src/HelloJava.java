fdab26b4e49638a77e7e226e39eebc413a5698b8
newdecide <liberty-1@nate.com>
2020-01-20 15:21:56 +0900

    [생성] 헬로 자바 프로젝트 생성

---
+++ 02_hellojava/HelloJava/src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("// Java 한줄 주석");
+        System.out.println("/* 여러줄 주석 */");
+    }
+}
