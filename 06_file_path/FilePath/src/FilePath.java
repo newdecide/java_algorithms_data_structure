79679391edda7fc83bb685d27b9952979bdd9cc7
newdecide <liberty-1@nate.com>
2020-01-25 23:58:17 +0900

    [추가] 폴더와 파일 확인하는 코드 추가

--- 06_file_path/FilePath/src/FilePath.java
+++ 06_file_path/FilePath/src/FilePath.java
@@ -9,5 +9,14 @@ public class FilePath {
         boolean isFile = file.isFile();
         System.out.println("파일이 있습니까? " + isFile);
         System.out.println("경로 구분자는 무엇입니까? " + File.separator);
+
+        File directory = new File("../../../java_algorithms_data_structure/06_file_path/FilePath");
+        boolean isDir = directory.isDirectory();
+        System.out.println("폴더입니까? " + isDir);
+
+        File nofile = new File("hello.java");
+        System.out.println("파일이 있습니다. "+ file.exists());
+        System.out.println("폴더가 있습니다. "+ directory.exists());
+        System.out.println("hello.java파일이 있습니까? " + nofile.exists());
     }
 }
