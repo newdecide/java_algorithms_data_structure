360d3a4d2090d98c962172c9253611d19f67291d
newdecide <liberty-1@nate.com>
2020-02-08 22:16:12 +0900

    [생성] 스트링 지정 문자열 반환

---
+++ 04_string_class/String_Class/src/StringSubstring.java
@@ -0,0 +1,7 @@
+public class StringSubstring {
+    public static void main(String[] args) {
+        String text = "text location";
+        String substring = text.substring(5,13);
+        System.out.println("substring : "+ substring);
+    }
+}
