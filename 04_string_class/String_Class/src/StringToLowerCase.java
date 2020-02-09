5fddb67a8006e9ffca06f4feb0c1c1961b717196
newdecide <liberty-1@nate.com>
2020-02-09 23:53:38 +0900

    [생성] 스트링 소문자 변환

---
+++ 04_string_class/String_Class/src/StringToLowerCase.java
@@ -0,0 +1,7 @@
+public class StringToLowerCase {
+    public static void main(String[] args) {
+        String lowercase = "Lowercase Change(Update)";
+        String toLowerCase = lowercase.toLowerCase();
+        System.out.println("toLowerCase : " + toLowerCase);
+    }
+}
