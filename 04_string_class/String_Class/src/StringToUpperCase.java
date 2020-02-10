f7bb81392212dccdff175e16ab397dae6d404568
newdecide <liberty-1@nate.com>
2020-02-10 23:01:38 +0900

    [생성] 스트링 대문자 변환

---
+++ 04_string_class/String_Class/src/StringToUpperCase.java
@@ -0,0 +1,7 @@
+public class StringToUpperCase {
+    public static void main(String[] args) {
+        String uppercase = "Uppercase Change(Update)";
+        String toUppercase = uppercase.toUpperCase();
+        System.out.println("toUpperCase : " + toUppercase);
+    }
+}
