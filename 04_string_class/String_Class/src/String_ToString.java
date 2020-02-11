de3390e6408034d7912cd7e80f06b80515cfe8cb
newdecide <liberty-1@nate.com>
2020-02-11 22:33:37 +0900

    [생성] 스트링 문자열로 변환

---
+++ 04_string_class/String_Class/src/String_ToString.java
@@ -0,0 +1,11 @@
+public class String_ToString {
+    public static void main(String[] args) {
+        String number = "1234";
+        String toString = number.toString();
+        System.out.println("toString = " + toString);
+
+        int number2 = 4321;
+        String toString2 = Integer.toString(number2);
+        System.out.println("toString2 = " + toString2);
+    }
+}
