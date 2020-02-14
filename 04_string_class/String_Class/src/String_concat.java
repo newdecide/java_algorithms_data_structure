321446c05a311ede2d90d6e235bb2d0a2ae25211
newdecide <liberty-1@nate.com>
2020-02-14 14:27:09 +0900

    [생성] 스트링 문자 결합

---
+++ 04_string_class/String_Class/src/String_concat.java
@@ -0,0 +1,8 @@
+public class String_concat {
+    public static void main(String[] args) {
+        String firsttext = "Character ";
+        String Lasttext = "combination";
+
+        System.out.println("첫번째 문자와 마지막 문자 결합 : "+ firsttext.concat(Lasttext));
+    }
+}
