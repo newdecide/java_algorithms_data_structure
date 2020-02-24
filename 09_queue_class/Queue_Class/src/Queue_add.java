bb7bbcb325b92294768beb0f16030c900140ec09
newdecide <liberty-1@nate.com>
2020-02-24 21:31:21 +0900

    [추가] 큐 클래스 데이터 추가

--- 09_queue_class/Queue_Class/src/Queue_add.java
+++ 09_queue_class/Queue_Class/src/Queue_add.java
@@ -2,6 +2,12 @@ import java.util.LinkedList;
 
 public class Queue_add {
     public static void main(String[] args) {
-        
+        LinkedList queue =new LinkedList();
+
+        for (int i = 5; i >= 1 ; i--){
+            queue.add(i);
+        }
+        System.out.println(queue);
+    }
     }
 }
