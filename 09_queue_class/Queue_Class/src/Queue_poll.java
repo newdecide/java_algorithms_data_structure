b0e9acb8738bab1d9358235b3e4604044d80e4b9
newdecide <liberty-1@nate.com>
2020-02-26 13:45:41 +0900

    [생성] 큐 클래스 첫번째 데이터 반환 및 제거

---
+++ 09_queue_class/Queue_Class/src/Queue_poll.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Queue_poll {
+    public static void main(String[] args) {
+        LinkedList queue = new LinkedList();
+        for (int i = 0; i < 5; i++) {
+            queue.add(i);
+        }
+
+        System.out.println("큐 =" + queue);
+        System.out.println("첫번째 데이터 반환 및 제거 : " + queue.poll());
+        System.out.println("현재 남아 있는 데이터 : "+queue);
+    }
+}
