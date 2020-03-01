5b750c698683fc862e428ff304c0dd27115d3852
newdecide <liberty-1@nate.com>
2020-03-01 23:06:19 +0900

    [생성] 정수형 타입 최댓값 최솟값

---
+++ 10_integer_class/Integer_Class/src/Integer_MaxMin.java
@@ -0,0 +1,6 @@
+public class Integer_MaxMin {
+    public static void main(String[] args) {
+        System.out.println("정수형 최대값 : " + Integer.MAX_VALUE);
+        System.out.println("정수형 최소값 : " + Integer.MIN_VALUE);
+    }
+}
