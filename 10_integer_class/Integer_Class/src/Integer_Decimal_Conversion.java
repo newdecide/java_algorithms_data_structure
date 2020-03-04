ff4ffe829513d8cccb6644e83245868318d7cebf
newdecide <liberty-1@nate.com>
2020-03-04 22:56:31 +0900

    [생성] 정수형 클래스 10진수 진법 변환

---
+++ 10_integer_class/Integer_Class/src/Integer_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+public class Integer_Decimal_Conversion {
+    public static void main(String[] args) {
+        System.out.println("<10진수 진법 변환>");
+        System.out.println("10진수 15 -> 2진수 : " + Integer.toBinaryString(15));
+        System.out.println("10진수 10 -> 8진수 : " + Integer.toOctalString(10));
+        System.out.println("10진수 26 -> 16진수 : " + Integer.toHexString(26));
+
+    }
+}
