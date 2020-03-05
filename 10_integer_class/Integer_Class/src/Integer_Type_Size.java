bbe3b22bb5e5f0dd132163cfa02d7e3e75671fde
newdecide <liberty-1@nate.com>
2020-03-05 19:55:36 +0900

    [생성] 정수형 클래스 데이터 타입과 비트수 확인

---
+++ 10_integer_class/Integer_Class/src/Integer_Type_Size.java
@@ -0,0 +1,7 @@
+public class Integer_Type_Size {
+    public static void main(String[] args) {
+        System.out.println("정수형 클래스 타입 확인1 : " + Integer.TYPE);
+        System.out.println("정수형 클래스 타입 확인2 : " + new Integer(23).TYPE);
+        System.out.println("정수형 클래스 비트 크기 확인 : " + Integer.SIZE);
+    }
+}
