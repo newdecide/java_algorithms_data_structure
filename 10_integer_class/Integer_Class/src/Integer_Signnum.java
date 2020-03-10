5543b3109d38a877be3f6b5fa5bd5b3bb4144542
newdecide <liberty-1@nate.com>
2020-03-10 23:30:40 +0900

    [생성] 정수형 클래스 부호 확인

---
+++ 10_integer_class/Integer_Class/src/Integer_Signnum.java
@@ -0,0 +1,7 @@
+public class Integer_Signnum {
+    public static void main(String[] args) {
+        System.out.println("부호가 양수면 1 음수면 -1");
+        System.out.println("100 : " + Integer.signum(100));
+        System.out.println("-100 : " + Integer.signum(-100));
+    }
+}
