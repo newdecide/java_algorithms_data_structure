350a1d8ef10ca8ce20aad1d2bf789d1abe9c8d3e
newdecide <liberty-1@nate.com>
2020-03-11 22:28:27 +0900

    [추가] 정수형 클래스 최댓값 최솟값 메서드

--- 10_integer_class/Integer_Class/src/Integer_MaxMin.java
+++ 10_integer_class/Integer_Class/src/Integer_MaxMin.java
@@ -2,5 +2,10 @@ public class Integer_MaxMin {
     public static void main(String[] args) {
         System.out.println("정수형 최댓값 : " + Integer.MAX_VALUE);
         System.out.println("정수형 최솟값 : " + Integer.MIN_VALUE);
+
+        System.out.println("최댓값 메서드");
+        System.out.println("125와 84중에 큰수 : " + Integer.max(125, 84));
+        System.out.println("최솟값 메서드");
+        System.out.println("125와 84중에 작은수 : " + Integer.min(125, 84));
     }
 }
