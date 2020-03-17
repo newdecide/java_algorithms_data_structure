ab0898130c688f6b3f6af13dcb4b705cae0edb61
newdecide <liberty-1@nate.com>
2020-03-17 15:49:02 +0900

    [추가] 정수형 클래스 오른쪽 쉬프트2 비트표현

--- 10_integer_class/Integer_Class/src/Integer_RotateRight.java
+++ 10_integer_class/Integer_Class/src/Integer_RotateRight.java
@@ -6,5 +6,11 @@ public class Integer_RotateRight {
         for (int shift=1; shift < 7; shift++){
             System.out.println("number 오른쪽 쉬프트[>>] : " + Integer.rotateRight(number,shift));
         }
+        System.out.println();
+
+        System.out.println("128오른쪽 쉬프트 비트 표현");
+        for (int shift=1; shift < 8; shift++){
+            System.out.println(Integer.rotateRight(number,shift)+" : " + Integer.toBinaryString(Integer.rotateRight(number,shift)));
+        }
     }
 }
