9f466af70c6a759bf2e22e4a4c11b77ef31402ed
newdecide <liberty-1@nate.com>
2020-03-19 20:43:09 +0900

    [생성] 정수형 클래스 2진수 최고 비트

---
+++ 10_integer_class/Integer_Class/src/Integer_HighestOneBit.java
@@ -0,0 +1,8 @@
+public class Integer_HighestOneBit {
+    public static void main(String[] args) {
+        int number = 200;
+        System.out.println(number + " 2진수 : " + Integer.toBinaryString(number));
+        System.out.println(number + "의 최고 비트수 : " + Integer.highestOneBit(number));
+
+    }
+}
