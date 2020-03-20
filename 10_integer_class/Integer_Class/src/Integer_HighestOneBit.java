e10225985c96995f92c14972aeebccdf530c282b
newdecide <liberty-1@nate.com>
2020-03-20 19:31:31 +0900

    [수정] 정수형 클래스 2진수 최고 비트 내용 변경

--- 10_integer_class/Integer_Class/src/Integer_HighestOneBit.java
+++ 10_integer_class/Integer_Class/src/Integer_HighestOneBit.java
@@ -2,7 +2,6 @@ public class Integer_HighestOneBit {
     public static void main(String[] args) {
         int number = 200;
         System.out.println(number + " 2진수 : " + Integer.toBinaryString(number));
-        System.out.println(number + "의 최고 비트수 : " + Integer.highestOneBit(number));
-
+        System.out.println(number + "의 최고 비트 : " + Integer.highestOneBit(number));
     }
 }
