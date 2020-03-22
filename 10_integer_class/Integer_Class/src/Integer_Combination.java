71f6b39c01f5e384ba734fc5c12251e09bd59c04
newdecide <liberty-1@nate.com>
2020-03-22 22:32:03 +0900

    [추가] 정수형 클래스 2진수 가운데 비트값 구하기

--- 10_integer_class/Integer_Class/src/Integer_Combination.java
+++ 10_integer_class/Integer_Class/src/Integer_Combination.java
@@ -3,11 +3,17 @@ public class Integer_Combination {
         int number = 359;
         System.out.println("비트의 개수 : " + Integer.bitCount(number));
         System.out.println("최저 비트 : " + Integer.lowestOneBit(number));
-
         int lowestbit = Integer.lowestOneBit(number);
-        System.out.println("최고 비트 : " + Integer.highestOneBit(number));
 
+        System.out.println("최고 비트 : " + Integer.highestOneBit(number));
         int highbit = Integer.highestOneBit(number);
+
         System.out.println("2진수 변환 : " + Integer.toBinaryString(number));
+
+        System.out.println("원데이터 : " + number);
+        System.out.println("최고 비트 + 최저 비트 : " + Integer.valueOf(highbit+lowestbit));
+        System.out.println("원데이터 - 최저 + 최고비트 : " + Integer.valueOf(number-highbit+lowestbit-2));
+        System.out.println("가운데 비트 변환 : " + Integer.toBinaryString(number-highbit+lowestbit-2));
+
     }
 }
