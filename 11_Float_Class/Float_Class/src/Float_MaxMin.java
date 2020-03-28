9e0cc8ad5073158649792e78eaff068252cca36d
newdecide <liberty-1@nate.com>
2020-03-28 23:03:34 +0900

    [생성] 실수형 클래스 최댓값 최솟값

---
+++ 11_Float_Class/Float_Class/src/Float_MaxMin.java
@@ -0,0 +1,7 @@
+public class Float_MaxMin {
+    public static void main(String[] args) {
+        System.out.println("실수형 최댓값 : " + Float.MAX_VALUE);
+        System.out.println("실수형 최솟값 : " + Float.MIN_VALUE);
+
+    }
+}
