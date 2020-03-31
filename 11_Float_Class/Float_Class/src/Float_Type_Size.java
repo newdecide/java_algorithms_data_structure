36e84828a4ea4d6ba38c4b2a967b5a7354a409ae
newdecide <liberty-1@nate.com>
2020-03-31 22:12:08 +0900

    [수정] 실수형 클래스 데이터 타입과 비트수 지수 확인 수정 및 추가

--- 11_Float_Class/Float_Class/src/Float_Type_Size.java
+++ 11_Float_Class/Float_Class/src/Float_Type_Size.java
@@ -2,6 +2,7 @@ public class Float_Type_Size {
     public static void main(String[] args) {
         System.out.println("실수형 클래스 타입 확인 : " + Float.TYPE);
         System.out.println("실수형 클래스 비트 크기 확인 : " + Float.SIZE);
-        System.out.println("실수형 클래스 지수 확인 : " + Float.MAX_EXPONENT);
+        System.out.println("실수형 클래스 최대지수 확인 : " + Float.MAX_EXPONENT);
+        System.out.println("실수형 클래스 최소지수 확인 : " + Float.MIN_EXPONENT);
     }
 }
