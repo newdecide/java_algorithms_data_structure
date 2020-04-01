bf3d73623fef2fb087c03a1f1ded9e56bd746054
newdecide <liberty-1@nate.com>
2020-04-01 22:56:04 +0900

    [생성] 실수형 클래스 기타 메서드

---
+++ 11_Float_Class/Float_Class/src/Float_Other_Method.java
@@ -0,0 +1,8 @@
+public class Float_Other_Method {
+    public static void main(String[] args) {
+        System.out.println("실수형 클래스 MIN_NORMAL : " + Float.MIN_NORMAL);
+        System.out.println("살슈횽 쿨래스 Nan : " + Float.NaN);
+        System.out.println("실수형 클래스 NEGATIVE_INFINITY : " + Float.NEGATIVE_INFINITY);
+        System.out.println("실수형 클래스 POSITIVE_INFINITY : " + Float.POSITIVE_INFINITY);
+    }
+}
