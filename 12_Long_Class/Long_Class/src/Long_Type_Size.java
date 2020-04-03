5f59ecb4704df93906327fafdd4d7a3e6d5cbd6b
newdecide <liberty-1@nate.com>
2020-04-03 21:53:02 +0900

    [생성] 정수형 클래스 데이터 타입과 비트수 확인

---
+++ 12_Long_Class/Long_Class/src/Long_Type_Size.java
@@ -0,0 +1,6 @@
+public class Long_Type_Size {
+    public static void main(String[] args) {
+        System.out.println("정수형 클래스(64비트) 타입 확인 : " + Long.TYPE);
+        System.out.println("정수형 클래스(64비트) 비트 크기 확인 : " + Long.SIZE);
+    }
+}
