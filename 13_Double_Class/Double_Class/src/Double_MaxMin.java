f34e90e6f259f7643519c1480c666e0ce7157db9
newdecide <liberty-1@nate.com>
2020-04-04 22:57:45 +0900

    [생성] 실수형 클래스(64비트) 최댓값 최솟값

---
+++ 13_Double_Class/Double_Class/src/Double_MaxMin.java
@@ -0,0 +1,6 @@
+public class Double_MaxMin {
+    public static void main(String[] args) {
+        System.out.println("실수형(64비트) 최댓값 : " + Double.MAX_VALUE);
+        System.out.println("실수형(64비트) 최솟값 : " + Double.MIN_VALUE);
+    }
+}
