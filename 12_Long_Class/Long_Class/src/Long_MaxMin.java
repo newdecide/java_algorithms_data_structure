6dad4ab200addaba70ed37fd2fd8bc923c76981d
newdecide <liberty-1@nate.com>
2020-04-05 22:55:33 +0900

    [수정] 정수형 클래스(64비트) 출력문 수정

--- 12_Long_Class/Long_Class/src/Long_MaxMin.java
+++ 12_Long_Class/Long_Class/src/Long_MaxMin.java
@@ -5,9 +5,9 @@ public class Long_MaxMin {
         System.out.println();
 
         System.out.println("정수형 큰값 비교 메서드");
-        System.out.println(""  + Long.max(124354423512342L, 23423465635636L));
+        System.out.println(Long.max(124354423512342L, 23423465635636L));
         System.out.println("정수형 작은 값 비교 메서드");
-        System.out.println( Long.min(124354423512342L, 23423465635636L));
+        System.out.println(Long.min(124354423512342L, 23423465635636L));
 
     }
 }
