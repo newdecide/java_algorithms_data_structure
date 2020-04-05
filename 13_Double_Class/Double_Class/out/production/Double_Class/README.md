63e52a7b14b5564b9f02ad9fcd4919c900a896ea
newdecide <liberty-1@nate.com>
2020-04-05 22:53:37 +0900

    [생성] 실수형 클래스(64비트) 최댓값 최솟값2

---
+++ 13_Double_Class/Double_Class/out/production/Double_Class/README.md
@@ -0,0 +1,4 @@
+# 11. 실수형 클래스(64비트)
+    1) 실수형 클래스 최댓값 최솟값
+        - Double.MAX_VALUE;
+        - Double.MIN_VALUE;
--- 13_Double_Class/Double_Class/src/Double_MaxMin.java
+++ 13_Double_Class/Double_Class/src/Double_MaxMin.java
@@ -2,5 +2,9 @@ public class Double_MaxMin {
     public static void main(String[] args) {
         System.out.println("실수형(64비트) 최댓값 : " + Double.MAX_VALUE);
         System.out.println("실수형(64비트) 최솟값 : " + Double.MIN_VALUE);
+        System.out.println("실수형(64비트) 큰값 비교 메서드");
+        System.out.println(Double.max(3450.1321323215656546446434, 2250.352345434355234));
+        System.out.println("실수형(64비트) 작은 값 비교 메서드");
+        System.out.println(Double.min(3450.1321323215656546446434, 2250.352345434355234));
     }
 }
