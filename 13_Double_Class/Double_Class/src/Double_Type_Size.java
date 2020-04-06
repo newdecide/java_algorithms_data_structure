8bec4c619f9676a7dc05be064da359ee380291ab
newdecide <liberty-1@nate.com>
2020-04-06 22:59:32 +0900

    [생성] 실수형 클래스(64비트) 데이터 타입과 비트수 확인

---
+++ 13_Double_Class/Double_Class/src/Double_Type_Size.java
@@ -0,0 +1,6 @@
+public class Double_Type_Size {
+    public static void main(String[] args) {
+        System.out.println("실수형 클래스(64비트) 타입 확인 : " + Double.TYPE);
+        System.out.println("실수형 클래스(64비트) 비트 크기 확인 : " + Double.SIZE);
+    }
+}
