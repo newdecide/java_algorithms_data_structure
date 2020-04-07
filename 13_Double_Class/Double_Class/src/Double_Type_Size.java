020d7390a6df057640c74d27e2125a36b7d62802
newdecide <liberty-1@nate.com>
2020-04-07 22:45:14 +0900

    [생성] 실수형 클래스(64비트) 데이터 타입 비트수 지수 확인

--- 13_Double_Class/Double_Class/src/Double_Type_Size.java
+++ 13_Double_Class/Double_Class/src/Double_Type_Size.java
@@ -2,5 +2,7 @@ public class Double_Type_Size {
     public static void main(String[] args) {
         System.out.println("실수형 클래스(64비트) 타입 확인 : " + Double.TYPE);
         System.out.println("실수형 클래스(64비트) 비트 크기 확인 : " + Double.SIZE);
+        System.out.println("실수형 클래스(64비트) 최대지수 확인 : " + Double.MAX_EXPONENT);
+        System.out.println("실수형 클래스(64비트) 최소지수 확인 : " + Double.MIN_EXPONENT);
     }
 }
