ec34f14d81fe042ca7e5e843707432601c1090e3
newdecide <liberty-1@nate.com>
2020-04-08 21:36:11 +0900

    [생성] 실수형 클래스(64비트) 기타 메서드

---
+++ 13_Double_Class/Double_Class/src/Double_Other_Method.java
@@ -0,0 +1,8 @@
+public class Double_Other_Method {
+    public static void main(String[] args) {
+        System.out.println("실수형 클래스(64비트) MIN_NORMAL : " + Double.MIN_NORMAL);
+        System.out.println("실수형 쿨래스(64비트) Nan : " + Double.NaN);
+        System.out.println("실수형 클래스(64비트) NEGATIVE_INFINITY : " + Double.NEGATIVE_INFINITY);
+        System.out.println("실수형 클래스(64비트) POSITIVE_INFINITY : " + Double.POSITIVE_INFINITY);
+    }
+}
