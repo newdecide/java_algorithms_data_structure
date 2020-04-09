d64ad89f163dba3c79af973d39072176abd15231
newdecide <liberty-1@nate.com>
2020-04-09 23:15:35 +0900

    [생성] 문자 클래스 확인 메서드

---
+++ 14_Character_Class/Character_Class/src/Character_isLetter.java
@@ -0,0 +1,7 @@
+public class Character_isLetter {
+    public static void main(String[] args) {
+        System.out.println("문자 클래스 확인 메서드 (Character 타입이 맞으면 true, 틀리면 false)");
+        System.out.println("c는 "+"문자가 맞나요? " + Character.isLetter('c'));
+        System.out.println("1은 "+"문자가 맞나요? " + Character.isLetter(1));
+    }
+}
