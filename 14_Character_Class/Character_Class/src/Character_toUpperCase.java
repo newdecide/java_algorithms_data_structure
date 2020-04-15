b2df0db7580b6f832ffd2fd996aa4b7c2fa87781
newdecide <liberty-1@nate.com>
2020-04-15 23:07:14 +0900

    [수정] 문자 클래스 대문자 변환 메서드 출력문 수정

--- 14_Character_Class/Character_Class/src/Character_toUpperCase.java
+++ 14_Character_Class/Character_Class/src/Character_toUpperCase.java
@@ -8,6 +8,6 @@ public class Character_toUpperCase {
         System.out.println("\'111\' 아스키 코드를 대문자로 변환 : " + Character.toUpperCase((char) 111));
         System.out.println();
         System.out.println("'\\u0072' 유니코드 변환 : " + '\u0072');
-        System.out.println("'\\u0072'유니코드룰 대문자로 변환 : " + Character.toUpperCase('\u0072'));
+        System.out.println("'\\u0072' 유니코드룰 대문자로 변환 : " + Character.toUpperCase('\u0072'));
     }
 }
