d388f5bad61ece950b79bf53cfdd7b204959da7d
newdecide <liberty-1@nate.com>
2020-04-16 23:39:18 +0900

    [생성] 문자 클래스 특수 문자

---
+++ 14_Character_Class/Character_Class/src/Character_SpecialChar.java
@@ -0,0 +1,14 @@
+public class Character_SpecialChar {
+    public static void main(String[] args) {
+        System.out.println("특수문자 사용");
+        System.out.println("tab : hello\ttab");
+        System.out.println("backspace : hello\b backspace");
+        System.out.println("form feed : hello\f formfeed");
+        System.out.println("줄바꿈 : hello \n newline");
+        System.out.println("carriage return : hello \r carriagereturn");
+        System.out.println("슬래시 사용 : hello \\(slash)");
+        System.out.println("작은 따옴표 사용 : \'hello\' Single quote");
+        System.out.println("큰 따옴표 사용 : hello \"Double quotes\"");
+        System.out.println("유니코드 사용 : " + '\u0068');
+    }
+}
