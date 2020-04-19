15ab0ab3638794388bf11ed742e0c9f6d589488b
newdecide <liberty-1@nate.com>
2020-04-19 16:50:39 +0900

    [추가] 문자 클래스 문자열의 문자값 보기2

--- 14_Character_Class/Character_Class/src/Character_codePointAt.java
+++ 14_Character_Class/Character_Class/src/Character_codePointAt.java
@@ -4,5 +4,12 @@ public class Character_codePointAt {
         for (int i = 0; i < charcode.length(); i++){
             System.out.println("CodePointAt("+i+") = " + charcode.codePointAt(i) + " : " +(char)charcode.codePointAt(i));
         }
+        System.out.println();
+
+        String charcode2 = "CodePointBefore";
+        for (int i = 1; i < charcode2.length()+1; i++){
+            System.out.println("CodePointBefore("+i+") = " + charcode2.codePointBefore(i) + " : " +(char)charcode2.codePointBefore(i));
+        }
+
     }
 }
