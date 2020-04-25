c20284c14639759707c63e18b780d3a048edbd44
newdecide <liberty-1@nate.com>
2020-04-25 22:20:44 +0900

    [생성] 문자 클래스 비교 메서드2

---
+++ 14_Character_Class/Character_Class/src/Character_Compare.java
@@ -0,0 +1,7 @@
+public class Character_Compare {
+    public static void main(String[] args) {
+        System.out.println("비교해서 같으면 0 아니면 -1");
+        System.out.println("a == a : " + Character.compare('a','a'));
+        System.out.println("a == b : " + Character.compare('a','b'));
+    }
+}
