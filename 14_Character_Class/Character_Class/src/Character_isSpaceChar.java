7b49519a60af7b362642afbaefd7c2a98df21ef6
newdecide <liberty-1@nate.com>
2020-04-26 22:39:00 +0900

    [생성] 문자 클래스 스페이스 문자 확인 메서드

---
+++ 14_Character_Class/Character_Class/src/Character_isSpaceChar.java
@@ -0,0 +1,8 @@
+public class Character_isSpaceChar {
+    public static void main(String[] args) {
+        System.out.println("[스페이스 문자 확인 메서드]");
+        System.out.println("스페이스 문자 인가요? " + Character.isSpaceChar(' '));
+        System.out.println("스페이스 문자 인가요? " + Character.isSpaceChar('a'));
+
+    }
+}
